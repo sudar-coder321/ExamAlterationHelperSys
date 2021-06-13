852d0a09c5d5624cf6bd29e4e585c30154adea23
shanthan6110 <70811852+shanthan6110@users.noreply.github.com>
2021-06-13 22:07:28 +0530

    Update tests.py

--- ExamAlterationHelperSys/examaltersys/tests.py
+++ ExamAlterationHelperSys/examaltersys/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from examaltersys.models import Exam, User_T, ExamAllocation, Room, AssignDuty, TakeDuty, Course, Notification, RoomAllocation
 from dateutil import tz
 from datetime import datetime
-
+print('pyunitmodel')
 utc_tz = tz.gettz('UTC')
 india_tz = tz.gettz('Asia/Kolkata')
 date_string = "2018-12-24T02:35:16-08:00"
